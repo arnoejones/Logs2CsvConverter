2bdde24ae856597beb227b38bee262eab24e3ab2
Arno Jones <r11sRider@gmail.com>
2019-01-09 09:48:58 -0800

    Final submission.  Handed off to Will Berry.

--- Logs2CsvConverter.py
+++ Logs2CsvConverter.py
@@ -9,9 +9,8 @@ import os
 import re
 import pandas as pd
 
-# print = sg.Print
-listbox_tuple = ()
-logfiles_list = []
+# listbox_tuple = ()
+# logfiles_list = []
 logfile_name = ''
 global myPath
 myPath = ''
@@ -32,20 +31,17 @@ class FileNotFoundException(Exception):
         return error_string
 
 
-def strip_nonascii(b):
-    z = str(b.decode('ascii', errors='ignore'))
-    return z
+# def strip_nonascii(b):
+#     z = str(b.decode('ascii', errors='ignore'))
+#     return z
 
 
-
-
-
-def list_to_tuple(listbox_contents):  # put in format the user interface can understand
-    listbox_tuple = ()
-    for item in listbox_contents:
-        listbox_tuple += (item,)
-
-    return listbox_tuple
+# def list_to_tuple(listbox_contents):  # put in format the user interface can understand
+#     listbox_tuple = ()
+#     for item in listbox_contents:
+#         listbox_tuple += (item,)
+#
+#     return listbox_tuple
 
 
 def create_empty_folder(folder_name):
@@ -73,46 +69,78 @@ def empty_dir(top):
 
 def convert_txt_to_csv(textfiles, output_file_name):
     pat = re.compile('[_\n ]*(?P<info>\S+)\s+\|\s+(?P<dt>\S+)\s+(?P<tm>\S+)\s+\|\s+(?P<app_name>.+)\n(?P<msg>.+)')
-    line2 = ''
+
     for logfile in textfiles:
         if os.path.getsize(logfile) > 0:
-            with open(logfile, "rb") as log_file:
-                t = log_file.read()
+            sum = []
+            with open(logfile, "rb") as log_file: # rb = read, binary.  This is the big one.  Reading text is better
+                                                    # because it reads ascii.  If you know for sure that your log
+                                                    # files will not contain binary data, then the command should
+                                                    # look like: with open(logfile, "r") as log_file:
+                for line in log_file:
+                    ascii_data = line.decode("ascii", errors="ignore") #look for binary (non-ascii) and ignore
+                    sum.append(ascii_data)  # take the new, decoded lines and add to the sum list. Strings are immutable.
+                str = ''.join(sum)  # turns a list into a string that we can regex against.
 
-                nline = strip_nonascii(t)
+                match = re.findall(pat, str)  # match is a list of tuples
 
-                match = re.findall(
-                    pat,
-                    nline)
-                df = pd.DataFrame(match)
+                # tuples are immutable, so we have to deconstruct them, make our changes, and reconstruct them.
+                tmp_list = []
+                for item in match:  # each list
+                    for tup in item:  # each tuple
+                        tmp_list.append(tup)  # list contains just items now, not tuples
+
+                for idx, l in enumerate(tmp_list):  # for each item in list
+                    if '\r' in l:  # remove the crlf if found
+                        tmp_list[idx] = l.replace('\r', ' ')  # now it's a modified list of changed values, but not tuples
+
+                counter = 0
+
+                # now we need to reconstruct a list of tuples.  BTW, each list item contains 5 tuples. Each tuple contains
+                # the category of info, such as the INFO, Date, Time, Message, etc.  This way each list item is one
+                # line in the spreadsheet.  In other words, the tuples are related by being a list item.  No need to
+                # comma-delimit or other shenanigans when your data already has commas or whatever.
+                final_tuple_list = []
+                temp_tuple_list = []
+                for list_item in tmp_list:
+                    # there is probably and elegant, pythonic way to do this, but I'm not experienced enough yet, so
+                    # here it is in agonizing detail.
+                    if counter <= 5:  # this is some hard-coding I hate.  Normally I would try to dynamically figure
+                        #out how many categories there are, but you promised there are only 5, so I'm taking the path
+                        # of least-resistance :-)
+                        temp_tuple_list.append(list_item)
+                        counter += 1
+                        if counter == 5:
+                            t = tuple(temp_tuple_list)
+                            final_tuple_list.append(t)
+                            counter = 0
+                            temp_tuple_list = []
+
+                # at this point we have a dumb array of data.  Let's smarten it up a bit anc conver the list of tuples
+                # into a Pandas dataframe.  Dataframes can be sophisticated and allow all kinds of programmatic
+                # customization.  For this task, the customization only consists of adding the column headers and
+                # and adding the log file names.
+                df = pd.DataFrame(final_tuple_list)
                 df.columns = ['Log Level', 'Date', 'Time', 'App Name', 'Message']
-
+                #let's add a new column name
                 df_log_name = pd.DataFrame(columns=['Log File Data Extracted From'])
+                # Fill that new column with the name of the source log file
                 df_log_name.loc[0] = logfile
-
+                # and finally, add it to the existing dataframe
                 df3 = df.assign(**df_log_name.iloc[0])
 
                 try:
                     with open(output_file_name, 'a') as f:
-                        df3.to_csv(f, mode='a', header=False)
+                        df3.to_csv(f, mode='a', header=True) # writing a dataframe to file is different the the
+                        #normal Python way.
                 except:
+                    # You should get this error message if you have the file open in Excel while you are trying to
+                    # write this file.  Excel (and others) put a lock on the file.  I won't know that until I get here.
                     sg.Print("Something is wrong with opening the {} file. Is it in use?").format(output_file_name)
-
-    #
-    #             for tup in match:
-    #                 tup_list = []
-    #                 for t in tup:
-    #                     tt = t.strip()
-    #                     tup_list.append(tt)
-    #                     tup_list.append(', ')
-    #                 tup_list.append('\n')
-    #                 for a in tup_list:
-    #                     print(a)
-    #                     csv_file.write(a)
 
     return True
 
-
+# https://pysimplegui.readthedocs.io/#how-do-i
 layout = [
     [sg.Text('Convert a Log File into a CSV File')],
     [sg.Text('Output CSV Filename'), sg.InputText('MyFileName.csv', do_not_clear=True)],
